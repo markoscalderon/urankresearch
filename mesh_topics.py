b360eb5a2f95fdb0d01d6b4ee9b979068c533bad
Markos Calderon <mcmarkos86@gmail.com>
2014-04-12 19:24:54 -0700

    Initial scripts

---
+++ mesh_topics.py
@@ -0,0 +1,51 @@
+#Script for getting top level mesh terms
+import csv
+from Bio import Entrez
+Entrez.email = "marcocal@usc.edu"
+
+#Data array, this will have the toplevels and the second levels
+data = []
+data.append(["rownum","toplevelid","toplevel_name","secondlevelid","secondlevel_name"])
+rownum = 0
+
+print data
+
+#Health occupations ID
+occ_handle = Entrez.esummary(db="mesh",id="68006281")
+occ_record = Entrez.read(occ_handle)
+
+#IDs of top level mesh terms
+#print occ_record[0]['DS_IdxLinks'][0]['Children']
+
+#Get toplevel terms
+for toplevelid in occ_record[0]['DS_IdxLinks'][0]['Children']:
+	#Toplevel name search
+	toplevel_handle = Entrez.esummary(db="mesh",id=toplevelid)
+	toplevel_record = Entrez.read(toplevel_handle)
+	toplevel_name = toplevel_record[0]['DS_MeshTerms'][0]
+	
+	#search for children IDs
+	toplevel_children = []
+	if len(toplevel_record[0]['DS_IdxLinks'][0]['Children'])>0:
+		toplevel_children = toplevel_record[0]['DS_IdxLinks'][0]['Children']
+		for secondlevelid in toplevel_children:
+			secondlevel_handle = Entrez.esummary(db="mesh",id=secondlevelid)
+			secondlevel_record = Entrez.read(secondlevel_handle)
+			secondlevel_name = secondlevel_record[0]['DS_MeshTerms'][0]
+			print toplevelid," ",toplevel_name," ",secondlevel_name
+			rownum+=1
+			data.append([rownum,toplevelid,toplevel_name,secondlevelid,secondlevel_name])	
+	else:
+		print toplevelid," ",toplevel_name
+		rownum+=1
+		data.append([rownum,toplevelid,toplevel_name,'',''])
+
+print data
+
+ofile = open("mesh.csv","wb")
+writer = csv.writer(ofile,delimiter='$',quotechar='"',quoting=csv.QUOTE_ALL)
+
+for row in data:
+	writer.writerow(row)
+
+ofile.close()
---
+++ papers_by_level.py
@@ -0,0 +1,63 @@
+#Script for getting the papers by mesh term
+import json
+
+from Bio import Entrez
+Entrez.email = "marcocal@usc.edu"
+
+# TODO: Get list of papers IDs, temporary fr Biomedical Engineering
+handle = Entrez.esearch(db="pubmed",term="Biomedical Engineering",sort="relevance",retmax=50)
+record = Entrez.read(handle)
+
+#iterate over papersID
+papers = []
+for paperID in record['IdList']:
+	paper_data = {}
+	paper_data['paper_id'] = paperID
+	paper_handle = Entrez.efetch(db="pubmed",id=paperID,retmode="xml")
+	paper_record = Entrez.read(paper_handle)
+	#check sample from out.txt
+	paper_data['paper_name'] = paper_record[0]['MedlineCitation']['Article']['ArticleTitle']
+
+	if paper_record[0]['MedlineCitation']['Article'].has_key('Abstract'):
+		paper_data['paper_abstract'] = paper_record[0]['MedlineCitation']['Article']['Abstract']
+	else:
+		paper_data['paper_abstract'] = "NA"
+	
+	for pubdate in paper_record[0]['PubmedData']['History']:
+		if pubdate.attributes['PubStatus'] == 'pubmed':
+			paper_data['paper_month'] = pubdate['Month']
+			paper_data['paper_day'] = pubdate['Day']
+			paper_data['paper_year'] = pubdate['Year']
+		else:
+			paper_data['paper_month'] = "NA"
+			paper_data['paper_day'] = "NA"
+			paper_data['paper_year'] = "NA"
+
+	paper_authors = []
+	# pauthor is the iterate over the authorlist of the xml
+	if paper_record[0]['MedlineCitation']['Article'].has_key('AuthorList'):
+		affiliation_default = "NA"
+		for pauthor in paper_record[0]['MedlineCitation']['Article']['AuthorList']:
+			paper_author = {}
+			if pauthor.has_key('LastName'):
+				if pauthor.has_key('ForeName'):
+					paper_author['name'] = pauthor['LastName'], " ", pauthor['ForeName']
+				elif pauthor.has_key('Initials'):
+					paper_author['name'] = pauthor['LastName'], " ", pauthor['Initials']
+				else:
+					paper_author['name'] = pauthor['LastName']
+			if pauthor.has_key('Affiliation'):
+				paper_author['affiliation'] = pauthor['Affiliation']
+				affiliation_default = pauthor['Affiliation']
+			else:
+				paper_author['affiliation'] = affiliation_default
+			paper_authors.append(paper_author)
+
+	paper_data['paper_authors'] = paper_authors
+
+	papers.append(paper_data)
+
+
+
+with open('papers.txt', 'wt') as out:
+	out.write(str(papers))
