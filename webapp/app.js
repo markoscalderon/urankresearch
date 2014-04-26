aaf4c01e383580d5c0d393f7fd3cd2668bae120f
Markos Calderon <mcmarkos86@gmail.com>
2014-04-26 16:13:05 -0700

    rest calls

--- webapp/app.js
+++ webapp/app.js
@@ -1,42 +1,48 @@
-/*var express = require('express');
-var app = express();
 
-var SparqlClient = require('sparql-client');
-var util = require('util');
-var endpoint = 'http://dbpedia.org/sparql';
- 
+/**
+ * Module dependencies.
+ */
 
- 
-app.get('/meshterms', function(req, res) {
-	res.send([{name:'wine1'}, {name:'wine2'}]);
+var express = require('express')
+  , routes = require('./routes')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
-app.get('/papers', function(req, res) {
-	var meshids = req.query.meshids;
 
-	res.send({name: "The Name", description: "description"});
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
- 
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/meshterms/toplevel',routes.meshtermtl);
+
+// example http://localhost:3000/meshterms/secondlevel?meshtermID=68016390
+app.get('/meshterms/secondlevel',routes.meshtermsl); //receive a toplevel meshID
+
+// example http://localhost:3000/ranking?meshtermIDs=68001698,68013677
+app.get('/ranking',routes.ranking); //receive a list of meshterms IDs
+
+// example http://localhost:3000/papers?meshtermIDs=68001698,68013677&university=Polytechnic+Institute+of+Milan
+app.get('/papers',routes.papers); //receive a list of meshterms IDs and a university
+
 app.listen(3000);
-console.log('Listening on port 3000...');*/
-
-
-var SparqlClient = require('sparql-client');
-var util = require('util');
-var endpoint = 'http://localhost:8080/openrdf-sesame/repositories/karma_data';
-
-// Get the leaderName(s) of the given citys
-// if you do not bind any city, it returns 10 random leaderNames
-var query = "SELECT ?meshid ?meshterm FROM <http://localhost:8080/openrdf-sesame/repositories/karma_data> WHERE { ?city <http://dbpedia.org/property/leaderName> ?leaderName } LIMIT 10";
-var client = new SparqlClient(endpoint);
-console.log("Query to " + endpoint);
-console.log("Query: " + query);
-client.query(query)
-  //.bind('city', 'db:Chicago')
-  //.bind('city', 'db:Tokyo')
-  //.bind('city', 'db:Casablanca')
-  .bind('city', '<http://dbpedia.org/resource/Vienna>')
-  .execute(function(error, results) {
-  process.stdout.write(results);
-});
-
-
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ webapp/routes/index.js
@@ -0,0 +1,258 @@
+var request = require('request');
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index')
+};
+
+/*
+app.get('/meshterms/toplevel',index.meshtermtl);
+app.get('/meshterms/secondlevel',index.meshtermsl); //receive a toplevel meshID
+app.get('/ranking',index.ranking); //receive a list of meshterms IDs
+app.get('/papers',index.papers); //receive a list of meshterms IDs and a university
+*/
+
+exports.meshtermtl = function(req, res){
+  console.log("meshtermtl");
+  var query_string = "PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#> " +
+  "select distinct ?meshid ?meshname " +
+  "WHERE { " +
+  "	?x urank:hasID ?meshid.  " +
+  "	?x urank:hasName ?meshname. " +
+  "	MINUS{ " +
+  "	?y urank:hasSubMeshTerms ?x " +
+  "	} " +
+  " }" +
+  " order by ?meshname ";
+
+  var options = {
+  	url: 'http://localhost:8080/openrdf-sesame/repositories/karma_data?query=' +encodeURIComponent(query_string),
+    headers: {
+      'Accept': 'application/sparql-results+json'
+    }
+  };
+
+  request(options, function(error, response, body){
+  	if (!error && response.statusCode == 200) {
+  		var json_res = JSON.parse(body);
+  		parseMeshterms(json_res,res);
+  	}
+  });  
+};
+
+exports.meshtermsl = function(req, res){
+  console.log("meshtermsl "+ req.query.meshtermID);
+
+  var query_string = "PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#> " +
+  "select distinct ?meshid ?meshname " +
+  "WHERE { " +
+  "	?x urank:hasID '" + req.query.meshtermID + "' ." +
+  "	?x urank:hasSubMeshTerms ?y. " +
+  "	?y urank:hasID ?meshid. " +
+  "	?y urank:hasName ?meshname ." +
+  " }" +
+  " order by ?meshname ";
+
+  var options = {
+  	url: 'http://localhost:8080/openrdf-sesame/repositories/karma_data?query=' +encodeURIComponent(query_string),
+    headers: {
+      'Accept': 'application/sparql-results+json'
+    }
+  };
+
+  request(options, function(error, response, body){
+  	console.log(error);
+  	console.log(response);
+  	console.log(body);
+  	if (!error && response.statusCode == 200) {
+  	  var json_res = JSON.parse(body);
+  	  parseMeshterms(json_res,res);
+  	}
+  	else{
+  	  res.status(404);
+  	}
+  });
+};
+/*
+PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#>
+select ?name (COUNT(?uni) AS ?count) 
+where{
+  ?x urank:hasID ?ids .
+  ?paper urank:hasMeshID ?x .
+  ?paper urank:hasUniversityAffiliation ?uni.
+  ?uni urank:hasName ?name
+  FILTER(?ids IN ("68001698","68013677"))
+}
+group by ?name
+*/
+exports.ranking = function(req, res){
+  console.log("ranking");
+  var ids = req.query.meshtermIDs;
+  var ids = '"' + ids.replace(/,/g,'","') + '"';
+
+  var query_string = "PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#> " +
+  "select ?name (COUNT(?uni) AS ?count) " +
+  "WHERE { " +
+  "	?x urank:hasID ?ids . " +
+  "	?paper urank:hasMeshID ?x . " +
+  "	?paper urank:hasUniversityAffiliation ?uni. " +
+  " ?uni urank:hasName ?name " +
+  " FILTER(?ids IN (" + ids + ")) " +
+  " }" +
+  " group by ?name ";
+
+  var options = {
+  	url: 'http://localhost:8080/openrdf-sesame/repositories/karma_data?query=' +encodeURIComponent(query_string),
+    headers: {
+      'Accept': 'application/sparql-results+json'
+    }
+  };
+
+  request(options, function(error, response, body){
+  	console.log(error);
+  	console.log(response);
+  	console.log(body);
+  	if (!error && response.statusCode == 200) {
+  	  var json_res = JSON.parse(body);
+  	  parseRanking(json_res,res);
+  	}
+  	else{
+  	  res.status(404);
+  	}
+  });
+};
+
+/*
+PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#>
+select ?title ?abstract ?author
+where{
+  ?x urank:hasID ?ids .
+  ?paper urank:hasMeshID ?x .
+  ?paper urank:hasTitle ?title.
+  ?paper urank:hasAbstract ?abstract.
+  ?paper urank:hasAuthor ?author
+  ?paper urank:hasUniversityAffiliation ?uni.
+  ?uni urank:hasName "Polytechnic Institute of Milan"
+  FILTER(?ids IN ("68001698","68013677"))
+}
+*/
+
+exports.papers = function(req, res){
+  console.log("papers");
+  var ids = req.query.meshtermIDs;
+  var ids = '"' + ids.replace(/,/g,'","') + '"';
+
+  var query_string = "PREFIX urank: <http://www.urank.com/ontologies/urankowl.owl#> " +
+  "select ?title ?abstract ?author " +
+  "WHERE { " +
+  "	?x urank:hasID ?ids . " +
+  "	?paper urank:hasMeshID ?x . " +
+  " ?paper urank:hasTitle ?title.  " +
+  " ?paper urank:hasAbstract ?abstract. " +
+  " ?paper urank:hasAuthor ?author. " +
+  "	?paper urank:hasUniversityAffiliation ?uni. " +
+  " ?uni urank:hasName '" + decodeURIComponent(req.query.university) + "' ." +
+  " FILTER(?ids IN (" + ids + ")) " +
+  " }";
+
+  var options = {
+  	url: 'http://localhost:8080/openrdf-sesame/repositories/karma_data?query=' +encodeURIComponent(query_string),
+    headers: {
+      'Accept': 'application/sparql-results+json'
+    }
+  };
+
+  request(options, function(error, response, body){
+  	console.log(error);
+  	console.log(response);
+  	console.log(body);
+  	if (!error && response.statusCode == 200) {
+  	  var json_res = JSON.parse(body);
+  	  parsePapers(json_res,res);
+  	}
+  	else{
+  	  res.status(404);
+  	}
+  });
+};
+
+
+function parseMeshterms(json_res, res) {
+  var resp = {};
+  resp.meshterms = [];
+
+  var vars = json_res.head.vars;
+  for (i in json_res.results.bindings) {
+  	var b = json_res.results.bindings[i];
+  	//console.log("\nrow "+i+" :");
+  	var id='', name='';
+
+  	for (j in vars) {
+  		var v = vars[j];
+  		//console.log(v+"="+b[v].value);
+  		if(v == "meshid")
+  			id = b[v].value;
+  		else if(v == "meshname")
+  			name = b[v].value;
+  	}
+  	resp.meshterms.push({ "id": id, "name":name });
+  }
+
+  //send the response back to the client
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(resp));  
+}
+
+function parseRanking(json_res, res) {
+  var resp = {};
+  resp.universities = [];
+
+  var vars = json_res.head.vars;
+  for (i in json_res.results.bindings) {
+  	var b = json_res.results.bindings[i];
+  	//console.log("\nrow "+i+" :");
+  	var uni='', count='';
+
+  	for (j in vars) {
+  		var v = vars[j];
+  		//console.log(v+"="+b[v].value);
+  		if(v == "name")
+  			uni = b[v].value;
+  		else if(v == "count")
+  			count = b[v].value;
+  	}
+  	resp.universities.push({ "university": uni, "count":count });
+  }
+
+  //send the response back to the client
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(resp));  
+}
+
+function parsePapers(json_res, res){
+  var resp = {};
+  resp.papers = {};
+
+  var vars = json_res.head.vars;
+  for (i in json_res.results.bindings) {
+  	var b = json_res.results.bindings[i];
+  	//console.log("\nrow "+i+" :");
+
+  	var title = b["title"].value;
+  	if(!(title in resp.papers)){
+  	  resp.papers[title] = {};
+  	  resp.papers[title].title = title;
+  	  resp.papers[title].abstract = b["abstract"].value;
+  	  resp.papers[title].authors = []; 	   
+  	}
+  	resp.papers[title].authors.push(b["author"].value);
+  }
+
+  //send the response back to the client
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(resp)); 
+}
+
