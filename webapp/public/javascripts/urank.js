f5a14216f824fa664cd0e5c98244c23052a03d65
Markos Calderon <mcmarkos86@gmail.com>
2014-04-26 23:25:44 -0700

    pie chart and table

--- webapp/public/javascripts/urank.js
+++ webapp/public/javascripts/urank.js
@@ -41,4 +41,94 @@ function displaysubterm (mainterm){
 
 }
 
+//Data Visualization
+google.load("visualization", "1", {packages:["corechart","table"]});
+google.setOnLoadCallback(drawChart);
+var chart = {};
+var data = [
+    ['University', 'Ranking'],
+    ['', 0]
+  ];
 
+function generateChart(){
+  var meshtermIDs = "68001698,68013677";
+
+  $.getJSON( "/ranking", { "meshtermIDs": meshtermIDs } )
+    .done(function( json ) {
+      var res = json.universities.sort(function(b,a) { return parseFloat(a.count) - parseFloat(b.count) } );
+      data = [
+        ['University', 'Ranking']
+      ];
+      counter = 0;
+      res.forEach(function(elem){
+        if(counter<10){
+          var row = [];
+          row.push(elem['university']);
+          row.push(parseInt(elem['count']));
+          data.push(row);  
+        }
+        counter+=1;
+      });
+      drawChart();
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+  //console.log(data);
+  
+}
+
+
+function drawChart() {
+  dataChart = google.visualization.arrayToDataTable(data);
+  console.log(dataChart);
+  var options = {
+    title: 'Top University Chart',
+    is3D: true,
+    backgroundColor: '#EEEEEE',
+    tooltip: { text: 'percentage'}
+  };
+  chart = new google.visualization.PieChart(document.getElementById('unichart'));
+  
+  chart.draw(dataChart, options);
+  google.visualization.events.addListener(chart, 'select', selectHandler);
+}
+
+
+function selectHandler(e) {
+  var uniselection = data[chart.getSelection()[0].row+1][0];
+  var meshtermIDs = "68001698,68013677";
+
+  $.getJSON( "/papers", { "meshtermIDs": meshtermIDs, "university":encodeURIComponent(uniselection) } )
+    .done(function( json ) {
+      console.log(json);
+      var rows = [];
+      for(paper in json.papers){
+        var row = [];
+        row.push(json.papers[paper].title);
+        row.push(json.papers[paper].abstract);
+        row.push(json.papers[paper].authors.toString());
+        row.push(json.papers[paper].pubmedURL);
+        rows.push(row); 
+      }
+      drawTable(rows);
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
+
+function drawTable(rows) {
+  var datatable = new google.visualization.DataTable();
+  datatable.addColumn('string', 'Title');
+  datatable.addColumn('string', 'Abstract');
+  datatable.addColumn('string', 'Authors');
+  datatable.addColumn('string', 'PubMed Link');
+  
+  datatable.addRows(rows);
+
+  var table = new google.visualization.Table(document.getElementById('tablepapers'));
+  table.draw(datatable, {showRowNumber: true});
+}
